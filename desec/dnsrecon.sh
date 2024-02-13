2f18abb7a73a79e33a4c1ca7d733d28185557ba7
jadsonmorais <morais.jadsonluiz@gmail.com>
2024-02-13 20:00:59 -0300

    dnsrecon.sh criado

--- desec/dnsrecon.sh
+++ desec/dnsrecon.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 #
-# Script de tentativa de transferencia de zona por dominio
+# Script para fazer pesquisa direta por dominio
 #
 # Autor:      Jadson Morais
 # Manutenção: Jadson Morais
 #
 # ------------------------------------------------------------------------ #
-#  O programa irá buscar dado um determinado alvo, buscar informações de infra
-#  com a tecnica de transferencia de zona
-#
 #  Exemplos:
-#      $ ./zone_transfer.sh businesscorp.com.br
+#      $ ./dnsrecon.sh businesscorp.com.br
 # ------------------------------------------------------------------------ #
 # Histórico:
 #
 #   v1.0 13/02/2024, Jadson:
 #       - Início do programa
 #       - Conta com as funcionalidades:
-#			- Consultar servidores NS do alvo
-#			- Tentar fazer a transferência de zona em cada servidor NS
+#			- Fazer brute force num determinado dominio 
 # ------------------------------------------------------------------------ #
 # Testado em:
 #   bash 5.2.21
@@ -58,12 +54,14 @@ done
 if [ -z "$TARGET" ]
 then
 	echo "O dominio alvo e obrigatorio. Use a opcao -d para definir."
+	echo "$MENSAGEM_USO"
 	exit 1
 fi
 
-for server in $(host -t ns $TARGET | cut -d " " -f4); do 
-	host -l -a $TARGET $server
-done
+for word in $(cat /etc/theHarvester/wordlists/names_small.txt); do 
+	host $word.$TARGET
+done | grep -v "NXDOMAIN"
 
 # ------------------------------------------------------------------------ #
+
 
